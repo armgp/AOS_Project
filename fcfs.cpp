a43a911b99cbe6217b0fa6555653db4a5e8a63c6
armgp <arm.gp23@gmail.com>
2022-11-25 17:15:43 +0530

    feedback queue v1

--- fcfs.cpp
+++ fcfs.cpp
@@ -182,6 +182,132 @@ void rr(vector<Process>& pList, int tq){
     pList = schedule;
 }
 
+void mlfq(vector<Process>& pList){
+    sort(pList.begin(), pList.end(), [](Process &p1, Process &p2){
+        return (p1.arrivalTime<p2.arrivalTime || (p1.arrivalTime==p2.arrivalTime && p1.pid<p2.pid));
+    });
+
+    vector<Process> schedule;
+
+    queue<Process> level1Q; //tq = 2
+    queue<Process> level2Q;
+    queue<Process> level3Q;
+    queue<Process> level4Q;
+
+    int ind = 1;
+    int n = pList.size();
+    int currTime = 0;
+    level1Q.push(pList[0]);
+    while(!level1Q.empty() || !level2Q.empty() || !level3Q.empty() || !level4Q.empty()){
+        
+        int tq1 = 2;
+        while(!level1Q.empty()){
+            Process currProcess = level1Q.front();
+            level1Q.pop();
+            currTime = max(currTime, currProcess.arrivalTime);
+            if(tq1 <= currProcess.burstTime){
+                currProcess.burstTime-=tq1;
+                currTime+=tq1;
+                currProcess.waitingTime-=tq1;
+            }else{
+                currTime+=currProcess.burstTime;
+                currProcess.waitingTime-=currProcess.burstTime;
+                currProcess.burstTime = 0;
+            }
+
+            while(ind<n && pList[ind].arrivalTime <= currTime){
+                level1Q.push(pList[ind++]);
+            }
+
+            if(currProcess.burstTime == 0){
+                currProcess.completionTime = currTime;
+                currProcess.turnAroundTime = currProcess.completionTime-currProcess.arrivalTime;
+                currProcess.waitingTime += currProcess.turnAroundTime;
+                schedule.push_back(currProcess);
+            }else{
+                level2Q.push(currProcess);
+            }
+        }
+
+        int tq2 = 5;
+        while(!level2Q.empty() && level1Q.empty()){
+            Process currProcess = level2Q.front();
+            level2Q.pop();
+            currTime = max(currTime, currProcess.arrivalTime);
+            if(tq2 <= currProcess.burstTime){
+                currProcess.burstTime-=tq2;
+                currTime+=tq2;
+                currProcess.waitingTime-=tq2;
+            }else{
+                currTime+=currProcess.burstTime;
+                currProcess.waitingTime-=currProcess.burstTime;
+                currProcess.burstTime = 0;
+            }
+
+            while(ind<n && pList[ind].arrivalTime <= currTime){
+                level1Q.push(pList[ind++]);
+            }
+
+            if(currProcess.burstTime == 0){
+                currProcess.completionTime = currTime;
+                currProcess.turnAroundTime = currProcess.completionTime-currProcess.arrivalTime;
+                currProcess.waitingTime += currProcess.turnAroundTime;
+                schedule.push_back(currProcess);
+            }else{
+                level3Q.push(currProcess);
+            }
+        }
+
+        int tq3 = 10;
+        while(!level3Q.empty() && level2Q.empty() && level1Q.empty()){
+            Process currProcess = level3Q.front();
+            level3Q.pop();
+            currTime = max(currTime, currProcess.arrivalTime);
+            if(tq3 <= currProcess.burstTime){
+                currProcess.burstTime-=tq3;
+                currTime+=tq3;
+                currProcess.waitingTime-=tq3;
+            }else{
+                currTime+=currProcess.burstTime;
+                currProcess.waitingTime-=currProcess.burstTime;
+                currProcess.burstTime = 0;
+            }
+
+            while(ind<n && pList[ind].arrivalTime <= currTime){
+                level1Q.push(pList[ind++]);
+            }
+
+            if(currProcess.burstTime == 0){
+                currProcess.completionTime = currTime;
+                currProcess.turnAroundTime = currProcess.completionTime-currProcess.arrivalTime;
+                currProcess.waitingTime += currProcess.turnAroundTime;
+                schedule.push_back(currProcess);
+            }else{
+                level4Q.push(currProcess);
+            }
+        }
+
+        if(!level4Q.empty() && level3Q.empty() && level2Q.empty() && level1Q.empty()){
+            vector<Process> fPlist;
+            while(!level4Q.empty()){
+                fPlist.push_back(level4Q.front());
+                level4Q.pop();
+            }
+
+            fcfs(fPlist);
+
+            for(Process p : fPlist){
+                schedule.push_back(p);
+            }
+        }
+
+
+    }
+
+    pList = schedule;
+
+}
+
 int main(){
 
     int n;
@@ -199,7 +325,8 @@ int main(){
     // fcfs(processes);
     // sjfNP(processes);
     // sjfP(processes);
-    rr(processes, 2);
+    // rr(processes, 2);
+    mlfq(processes);
 
     for(Process p : processes) p.showPocess();
     
