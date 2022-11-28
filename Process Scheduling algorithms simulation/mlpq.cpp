a8e832e0fec937b705e76a445b028c9126c60880
armgp <arm.gp23@gmail.com>
2022-11-28 18:04:37 +0530

    mlpq error

--- Process Scheduling algorithms simulation/mlpq.cpp
+++ Process Scheduling algorithms simulation/mlpq.cpp
@@ -352,10 +352,6 @@ vector<Process> mlpq(vector<Process> newProcesses){
 }
 
 int main(){
-    #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-    #endif
 
     int n;
     cin>>n;
